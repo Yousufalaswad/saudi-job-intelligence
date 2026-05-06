677394be46be548658c80e342597af1cb3c917b1
Yousufalaswad <aswadyousef55@gmail.com>
2026-05-05 20:54:47 -0400

    Fix Streamlit secrets syntax

--- database.py
+++ database.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 try:
     import streamlit as st
-    SUPABASE_URL = st.secrets.get("SUPABASE_URL") or os.getenv("SUPABASE_URL")
-    SUPABASE_KEY = st.secrets.get("SUPABASE_KEY") or os.getenv("SUPABASE_KEY")
-except:
+    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+except Exception:
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
